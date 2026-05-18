52b32d94726b55104726dae2a0dab6c07384fa2c
Maicon <pedro_poa87@hotmail.com>
2026-05-17 23:56:32 -0300

    Add Streamlit app to display DataFrame from CSV

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import streamlit as st
+import pandas as pd
 
+st.title("Leitura do Banco de Dados")
+
+url = "https://raw.githubusercontent.com/amdbraz/PI_G36/refs/heads/main/dataset_tratado.csv"
+
+df = pd.read_csv(url)
+
+st.dataframe(df)
